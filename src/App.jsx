918a5a0fd4b8a64356ab9b68f3546b471e19a09b
github kazu-mita <kazu0726m@icloud.com>
2021-10-17 20:56:03 +0900

    ＃初期設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import React from "react";
-import defaultDataset from "./data";
-import './assets/styles/style.css'
+import defaultDataset from "./dataset";
+import "./assets/styles/style.css";
+import { AnswersList } from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          answer: [],
-          chats: [],
-          currentId: "init",
-          dataset: defaultDataset,
-          open: false
-      }
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false,
+    };
   }
+
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+    this.setState({
+      answers: initAnswers,
+    });
+  };
+
+  componentDidMount() {
+    this.initAnswer();
+  }
+
   render() {
     return (
       <div>
         <section className="c-section">
-            <div className="c-box">
-              {this.state.currentId}
-            </div>
+          <div className="c-box">
+            <AnswersList answers={this.state.answers} />
+          </div>
         </section>
       </div>
     );
---
+++ src/components/Answer.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const Answer = (props) => {
+  // const classes = useStyles;
+  return (
+    <Button variant="contained" color="primary">
+      {props.content}
+    </Button>
+  );
+};
+export default Answer;
